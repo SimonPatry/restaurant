98e986d0b7c8b0b3503cc49f041afae75e1e5c8c
SimonPATRY <simonpatry12@gmail.com>
2021-05-07 14:06:03 +0000

    transition dashboard ajax

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -12,14 +12,20 @@ class DashboardController
 {
 	private $model;
 	public $message;
+	private $categories;
 	public function __construct()
 	{
-		
+		$this -> model = new \Models\Categories();
+	}
+	
+	public function displayCategories()
+	{
+	    $categoriesTable = $this -> categories -> getAllCategories();
+		include "views/dashboard.phtml";
 	}
 	public function display()
 	{
 		$template = "views/dashboard.phtml";
 		include 'views/layout.phtml';
 	}
-	
 }
