36b8416fa9694a21f827d1eee5772c91643375ac
GuilhemDenis <guilhem.denis@gmail.com>
2021-05-07 10:40:07 +0000

    Ajout des methodes users et booking

--- models/Booking.php
+++ models/Booking.php
@@ -4,5 +4,18 @@ namespace Models;
 
 class Booking extends Database
 {
+    public function getAllBookings():array
+    {
+        return $this -> findAll("
+    	SELECT customer_number, id_user, date, hour, status, comment
+    	FROM users");
+    }
     
+    public function getBookingByUser(int $id):array
+    {
+        return $this -> findOne("
+    	SELECT customer_number, id_user, date, hour, status, comment
+    	FROM users
+    	WHERE id_user = ?", [$id]);
+    }
 }
--- models/Users.php
+++ models/Users.php
@@ -4,5 +4,18 @@ namespace Models;
 
 class Users extends Database
 {
+    public function getAllUsers():array
+    {
+        return $this -> findAll("
+    	SELECT password, email, first_name, last_name, phone
+    	FROM admin");
+    }
     
+    public function getUserByEmail(string $email):array
+    {
+        return $this -> findOne("
+    	SELECT password, email, first_name, last_name, phone
+    	FROM admin
+    	WHERE email = ?", [$email]);
+    }
 }
